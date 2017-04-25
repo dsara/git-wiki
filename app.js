01bba05ac8d5a43c57c2bfc0904b4b1cc99f03af
dsara <dan.steve.sara@gmail.com>
2017-04-24 22:37:46 -0600

    add locals

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.locals.devDeployVersion = (new Date).getTime();
+app.locals.prodDeployVersion = "0.0.1";
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
