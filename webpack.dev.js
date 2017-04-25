2f7e6f627c256b4c0293a52c0bef2a6989b747dc
dsara <dan.steve.sara@gmail.com>
2017-04-25 09:41:18 -0600

    add node dev envion variable

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,10 @@
 var webpackMerge = require('webpack-merge');
+var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var commonConfig = require('./webpack.common.js');
 var helpers = require('./config/helpers');
+
+const ENV = process.env.NODE_ENV = process.env.ENV = 'development';
 
 module.exports = webpackMerge(commonConfig, {
   devtool: 'cheap-module-eval-source-map',
@@ -16,7 +19,6 @@ module.exports = webpackMerge(commonConfig, {
   plugins: [
     new ExtractTextPlugin('stylesheets/[name].dev.css')
   ],
-
   devServer: {
     historyApiFallback: true,
     stats: 'minimal'
