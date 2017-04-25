7e675c43002a583fbda74e6c83fc721e7cb02bd6
dsara <dan.steve.sara@gmail.com>
2017-04-24 21:08:26 -0600

    load pug file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ViewContainerRef } from "@angular/core";
 
 @Component({
     selector: "git-wiki",
-    templateUrl: "app.component.html"
+    templateUrl: "app.component.pug"
 })
 
 export class AppComponent implements OnInit {
