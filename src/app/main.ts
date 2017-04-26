e96ba40980a93677e6384c8220a332a3b18af4ec
dsara <dan.steve.sara@gmail.com>
2017-04-25 21:15:10 -0600

    moving files down

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,22 +1,23 @@
 import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
 import {ErrorHandler, NgModule} from "@angular/core";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {MdButtonModule} from "@angular/material";
 import {BrowserModule} from "@angular/platform-browser";
 import {ReactiveFormsModule, FormsModule} from "@angular/forms";
 import {LocationStrategy, HashLocationStrategy} from "@angular/common";
 import {HttpModule} from "@angular/http";
 import {RouterModule} from "@angular/router";
 
-import {AppComponent} from "./app/app.component";
+import {AppComponent} from "./app.component";
 
 import "rxjs";
 
 @NgModule({
-    imports: [BrowserModule, HttpModule],
+    imports: [BrowserModule, BrowserAnimationsModule, MdButtonModule, FlexLayoutModule, HttpModule],
     declarations: [AppComponent],
     bootstrap: [AppComponent]
 })
 class GitWikiModule {}
 
-document.addEventListener("DOMContentLoaded", ev => {
-    platformBrowserDynamic().bootstrapModule(GitWikiModule);
-});
+platformBrowserDynamic().bootstrapModule(GitWikiModule);
---
+++ src/app/vendor.ts
@@ -0,0 +1,18 @@
+import '@angular/platform-browser';
+import '@angular/platform-browser-dynamic';
+import '@angular/core';
+import '@angular/common';
+import '@angular/http';
+import '@angular/router';
+import "@angular/platform-browser/animations";
+import "@angular/material";
+import "@angular/flex-layout";
+
+import 'rxjs';
+
+import 'jquery';
+
+require("../../node_modules/bootstrap/dist/css/bootstrap.css");
+require('bootstrap');
+
+require("../../node_modules/@angular/material/prebuilt-themes/deeppurple-amber.css");
