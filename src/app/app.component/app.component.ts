7b79ba5e2dc8748c25f1e674733b68338fad4736
dsara <dan.steve.sara@gmail.com>
2017-04-28 13:48:30 -0600

    add sass reference

--- src/app/app.component/app.component.ts
+++ src/app/app.component/app.component.ts
@@ -2,10 +2,13 @@ import { Component, OnInit, ViewContainerRef } from "@angular/core";
 
 @Component({
     selector: "git-wiki",
-    templateUrl: "app.component.pug"
+    templateUrl: "app.component.pug",
+    styleUrls: ["app.component.scss"]
 })
 
 export class AppComponent implements OnInit {
+    private markdownSource: string = "";
+    
     constructor() {
 
     }
