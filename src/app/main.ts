f4e6d5c260a9014d205540485d9d946202ca8050
dsara <dan.steve.sara@gmail.com>
2017-04-28 13:47:20 -0600

    add new modules

--- src/app/main.ts
+++ src/app/main.ts
@@ -2,19 +2,31 @@ import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
 import {ErrorHandler, NgModule} from "@angular/core";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {FlexLayoutModule} from "@angular/flex-layout";
-import {MdButtonModule} from "@angular/material";
+import {MdButtonModule, MdSidenavModule, MdToolbarModule, MdInputModule} from "@angular/material";
 import {BrowserModule} from "@angular/platform-browser";
 import {ReactiveFormsModule, FormsModule} from "@angular/forms";
 import {LocationStrategy, HashLocationStrategy} from "@angular/common";
 import {HttpModule} from "@angular/http";
 import {RouterModule} from "@angular/router";
+import {MarkdownToHtmlModule} from 'markdown-to-html-pipe';
 
 import {AppComponent} from "./app.component";
 
 import "rxjs";
 
+require("../styles/global.scss");
+
 @NgModule({
-    imports: [BrowserModule, BrowserAnimationsModule, MdButtonModule, FlexLayoutModule, HttpModule],
+    imports: [BrowserModule,
+              BrowserAnimationsModule,
+              MdButtonModule,
+              MdSidenavModule,
+              MdInputModule,
+              MdToolbarModule,
+              FlexLayoutModule,
+              FormsModule,
+              HttpModule,
+              MarkdownToHtmlModule],
     declarations: [AppComponent],
     bootstrap: [AppComponent]
 })
