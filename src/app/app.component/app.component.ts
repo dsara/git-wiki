6dfde85876e9aede485b200e1cddd50e5ec4c123
dsara <dan.steve.sara@gmail.com>
2017-04-29 17:11:33 -0600

    get and set services

--- src/app/app.component/app.component.ts
+++ src/app/app.component/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ViewContainerRef } from "@angular/core";
+import {IWikiPage} from "../../interfaces";
+import {WikiPageService} from '../shared/services';
 
 @Component({
     selector: "git-wiki",
@@ -9,11 +11,41 @@ import { Component, OnInit, ViewContainerRef } from "@angular/core";
 export class AppComponent implements OnInit {
     private markdownSource: string = "";
     
-    constructor() {
+    constructor(private wikiPageService: WikiPageService) {
 
     }
 
     ngOnInit(): void {
+        this.wikiPageService.getWikiPage('New Title')
+            .subscribe(
+                (wikiPage: IWikiPage) => {
+                    if (wikiPage) {
+                        this.markdownSource = wikiPage.content;
+                    }
+                },
+                (err: any) => {
+                    console.error(err);
+                }
+            )
+    }
 
+    saveWikiPage() {
+        var wikiPage: IWikiPage = <IWikiPage>{};
+
+        wikiPage.content = this.markdownSource;
+        wikiPage.name = "New Title";
+        wikiPage.created = Date.now().toString();
+        wikiPage.modified = Date.now().toString();
+        wikiPage.version = 1;
+
+        this.wikiPageService.createWikiPage(wikiPage)
+            .subscribe(
+                (resWikiPage: IWikiPage) => {
+                    console.log("created new wiki", resWikiPage);
+                },
+                (err: any) => {
+                    console.error(err);
+                }
+            );
     }
 };
