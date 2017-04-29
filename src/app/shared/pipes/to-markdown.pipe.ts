057f5d22fe159cf2cda921353dffa974192f04ba
dsara <dan.steve.sara@gmail.com>
2017-04-29 00:00:44 -0600

    specific code rendering

--- src/app/shared/pipes/to-markdown.pipe.ts
+++ src/app/shared/pipes/to-markdown.pipe.ts
@@ -1,14 +1,25 @@
 import * as marked from 'marked';
 import { Pipe, PipeTransform } from '@angular/core';
-import * as highlight from 'highlight.js';
+import * as hljs from 'highlight.js';
 
 @Pipe({ name: 'mdToHtml' })
 export class MDToHtmlPipe implements PipeTransform {
+    private renderer = new marked.Renderer();
+
+    constructor() {
+        this.renderer.code = function(code, language): string {
+            return '<pre class="hljs"><code>' +
+                (language ? (hljs.getLanguage(language) ? hljs.highlight(language, code).value : hljs.highlightAuto(code).value) : hljs.highlightAuto(code).value) +
+                '</code></pre>';
+        }
+    }
+
     transform(markdown: string): string {
         return marked(markdown, {
-            highlight: function (code) {
-                return highlight.highlightAuto(code).value;
-            }
+            renderer: this.renderer
+            // highlight: function (code) {
+            //     return highlight.highlightAuto(code).value;
+            // }
         });
     }
 }
