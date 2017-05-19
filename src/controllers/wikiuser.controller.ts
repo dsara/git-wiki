4d628641ef6c218c6703833d0932b0728eb090fc
dsara <dan.steve.sara@gmail.com>
2017-05-19 16:20:44 -0600

    cleanup and add logout

--- src/controllers/wikiuser.controller.ts
+++ src/controllers/wikiuser.controller.ts
@@ -15,89 +15,35 @@ var LocalStrategy = passportLocal.Strategy;
 
 export class WikiUser {
 
-    // static login(req: express.Request, res: express.Response) {
-    //     passport.authenticate('local', (err, user, info) => {
-    //         var token;
-
-    //         if (err) {
-    //             res.status(404).json(err);
-    //             return;
-    //         }
-
-    //         if (user) {
-    //             token = user.generateJwt();
-    //             res.status(200);
-    //             res.json({
-    //                 'token': token
-    //             });
-    //         } else {
-    //             res.status(401).json(info);
-    //         }
-    //     })(req, res, function(){});
-    // }
-
     static isLoggedIn(req: express.Request, res: express.Response, next) {
         if (req.isAuthenticated()) {
             return next();
         }
 
-        res.status(401);
+        res.status(401).json({ error: 'You are not logged in'});
     }
 
-    // static register(req: express.Request, res: express.Response) {
-    //     passport.authenticate('local-signup', (err, user, info) => {
-    //         var token;
+    static logOut(req: express.Request, res: express.Response, next) {
+        req.logOut();
 
-    //         if (err) {
-    //             res.status(404).json(err);
-    //             return;
-    //         }
-
-    //         if (user) {
-    //             token = user.generateJwt();
-    //             res.status(200);
-    //             res.json({
-    //                 'token': token
-    //             });
-    //         } else {
-    //             res.status(401).json(info);
-    //         }
-    //     });
-    // }
-
-    // static register(req: express.Request, res: express.Response) {
-    //     var user = new wikiUserModel();
-
-    //     user.name = req.body.name;
-    //     user.email = req.body.email;
-
-    //     user.hash = user.setPassword(req.body.password);
-    //     // user.setPassword(req.body.password);
-
-    //     user.save((err) => {
-    //         var token;
-    //         token = user.generateJwt();
-    //         res.status(200);
-    //         res.json({
-    //             'token': token
-    //         });
-    //     });
-    // }
+        res.status(200).json({ message: 'Logged Out' });
+    }
 
     static register(req: express.Request, res: express.Response) {
         wikiUserModel.register(new wikiUserModel({ username: req.body.username }), req.body.password, function (err, account) {
             if (err) {
-                return res.render('register', {info: 'Sorry, that username already exists. Try again.'});
+                res.status(409).json({ message: 'Sorry, that username already exists. Try again.'});
+                return;
             }
 
             passport.authenticate('local')(req, res, function() {
-                res.status(200).json({ done: 'authenticated' });
+                res.status(200).json({ login: { status: 'authenticated', user: req.body.username } });
                 //res.redirect('/');
             });
         });
     }
 
     static login(req: express.Request, res: express.Response) {
-        res.status(200);
+        res.status(200).json({ login: { status: 'authenticated', user: req.body.username } });
     }
 }
